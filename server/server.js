42aa5f93402adb7ceed3d1ad961c737b0ee73ebc
Abhinavjs903 <abhinavmait300@gmail.com>
2026-07-09 23:14:12 +0530

    Completed authentication and mongo db works h

---
+++ server/server.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const connectDB = require("./config/db");
+connectDB();
+const express = require("express");
+
+const authRoutes = require("./routes/authRoutes");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+
+    res.send("🚀 Server is running successfully!");
+
+});
+
+app.post("/login", (req, res) => {
+
+    const { email, password } = req.body;
+
+    if (email === "admin@mcd.gov.in" && password === "34567890-") {
+
+        res.json({
+            success: true,
+            message: "Login Successful!"
+        });
+
+    } else {
+
+        res.json({
+            success: false,
+            message: "Invalid Email or Password"
+        });
+
+    }
+
+});
+
+app.listen(PORT, () => {
+
+    console.log(`Server is running on http://localhost:${PORT}`);
+
+});
