1abbc21a2ad1ef6e614b595f9ec2bc43c704669f
Abhinavjs903 <abhinavmait300@gmail.com>
2026-07-22 14:19:14 +0530

    updated

--- client/js/config.js
+++ client/js/config.js
@@ -1,4 +1,5 @@
 const API_URL =
-window.location.hostname === "localhost"
-    ? "http://localhost:5000"
-    : "https://secure-auth-357h.onrender.com/";
+    window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1"
+        ? "http://localhost:5000"
+        : "https://secure-auth-357h.onrender.com";
